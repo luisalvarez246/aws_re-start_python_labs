556246bcfd0fb0c2e121926960faf89fb303a069
luisalvarez246 <luisalvarez1@correo.unimet.edu.ve>
2023-12-01 11:14:21 +0000

    add collections exercises

---
+++ collections.py
@@ -0,0 +1,17 @@
+my_fruit_list = ["apple", "banana", "cherry"];
+my_final_answer_tuple = ("apple", "banana", "cherry");
+my_favorite_fruit_dictionary = {
+    "Akua": "apple",
+    "Saavi": "banana",
+    "Paulo": "pineapple"
+}
+
+my_fruit_list[2] = "orange";
+my_fruit_list.append(1);
+print(my_fruit_list);
+print(type(my_fruit_list));
+print(my_final_answer_tuple)
+print(type(my_final_answer_tuple));
+print(my_favorite_fruit_dictionary);
+print(type(my_favorite_fruit_dictionary));
+print(my_favorite_fruit_dictionary["Paulo"]);
